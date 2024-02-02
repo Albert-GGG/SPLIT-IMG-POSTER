8a33073784a7ba5e7994f7ef65c0c7c1df764667
Albert <lggaliano05@outlook.com>
2024-02-01 23:34:36 -0600

    new mods

--- crop.py
+++ crop.py
@@ -5,7 +5,7 @@ from fpdf import FPDF
 
 ##########################| CONFIGURATION |#############################
 
-img_name = 'books.png'
+img_name = 'forest.jpg'
 path_img = './IMGS' + '/' + img_name
 
 img = cv.imread(path_img)
@@ -16,6 +16,7 @@ if img is None:
 rows = int(input('Number of rows: '))
 columns = int(input('Number of columns: '))
 
+# Regions height and width in pixels
 height_region, width_region = int(img.shape[0] / rows), int(img.shape[1] / columns)
 
 # Name of new folder with split image
@@ -24,8 +25,8 @@ path = './cropped_' + img_name.split('.')[0]
 if not os.path.exists(path):
     os.mkdir(path)
 
+# Split the image in regions and add them to a list
 imgs = list()
-
 for h in range(rows):
     for w in range(columns):
         new_region = img[h * height_region:height_region * (h + 1), w * width_region:width_region * (w + 1)]
@@ -35,25 +36,38 @@ for h in range(rows):
 for i in range(len(imgs)):
     cv.imwrite(path + '/' + f'parte_{i + 1}.png', imgs[i])
 
-orientation = None
-pdf_width = None
-mm = 3.7795275591
+
+# Aspect ratios of the area for the image and the image itself
+area_aspect_ratio = 270 / 205
+region_aspect_ratio = width_region / height_region
+
+# Default values of area occupied by the image region
+area_width = 0
+area_height = 0
 
 # Add image with maximum size to a letter size page inside a PDF
-if width_region / height_region > 1:
+if region_aspect_ratio >= 1:
     orientation = 'L'
-    pdf_width = 270
+    if region_aspect_ratio > area_aspect_ratio:
+        area_width = 270
+    else:
+        area_height = 205
 else:
     orientation = 'P'
-    pdf_width = 205
-
+    if region_aspect_ratio < area_aspect_ratio:
+        area_height = 270
+    else:
+        area_width = 205
+    
+# Initialize PDF document of letter size
 pdf = FPDF(orientation=orientation, unit='mm', format='Letter')
 
+# Add a page for each image
 for el in os.listdir(path):
     if el.split('.')[1] != 'png':
         continue
     pdf.add_page()
-    pdf.image(path + '/' + el, x = 5, y = 5, w = pdf_width)
+    pdf.image(path + '/' + el, x = 5, y = 5, w = area_width, h = area_height)
 
 pdf.output(path + '/' + 'cropped.pdf')
     
